a3413d879b5a423e268673c1d417000567d878c6
andy-beck <andynbeck@gmail.com>
2016-02-16 10:02:43 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+'use strict';
+
 /* JAVASCRIPT FUNCTIONS */
 function imgLoaded(img) {
+   //console.log(img);
    var imgWrapper = img.parentNode;
    imgWrapper.className += imgWrapper.className ? ' loaded' : 'loaded';
 };
@@ -16,74 +19,130 @@ var app = angular.module('app', [
 
 
 app.config(['$routeProvider',
-  function ($routeProvider) {
-     $routeProvider.
-       when('/gallery/:itemCategory', {
-          templateUrl: 'partials/list.html',
-          controller: 'ListCtrl'
-       }).
-       when('/gallery/:itemCategory/:itemUrl', {
-          templateUrl: 'partials/detail.html',
-          controllerAs: 'detail',
-          controller: 'DetailCtrl'
-       }).
-       when('/about', {
-          title: 'About',
-          templateUrl: 'partials/about.html',
-          controller: 'PageCtrl'
-       }).
-       when('/news', {
-          title: 'News',
-          templateUrl: 'partials/news.html',
-          controller: 'PageCtrl'
-       }).
-       when('/contact', {
-          title: 'Contact',
-          templateUrl: 'partials/contact.html',
-          controller: 'ContactCtrl'
-       }).
-       otherwise({
-          redirectTo: '/',
-          templateUrl: 'partials/home.html',
-          controller: 'PageCtrl'
-       });
-  }
+   function ($routeProvider) {
+      $routeProvider.
+      when('/gallery/:itemCategory', {
+         templateUrl: 'partials/list.html',
+         controller: 'ListCtrl'
+      }).
+      when('/gallery/:itemCategory/:itemUrl', {
+         templateUrl: 'partials/detail.html',
+         controllerAs: 'detail',
+         controller: 'DetailCtrl'
+      }).
+      when('/about', {
+         title: 'About',
+         templateUrl: 'partials/about.html',
+         controller: 'PageCtrl'
+      }).
+      when('/news', {
+         title: 'News',
+         templateUrl: 'partials/news.html',
+         controller: 'PageCtrl'
+      }).
+      when('/contact', {
+         title: 'Contact',
+         templateUrl: 'partials/contact.html',
+         controller: 'ContactCtrl'
+      }).
+      otherwise({
+         redirectTo: '/',
+         templateUrl: 'partials/home.html',
+         controller: 'PageCtrl'
+      });
+   }
 ]);
 
 
-app.directive("navBar", function () {
+/* DIRECTIVES */
+app.directive('navBar', function () {
    return {
-      templateUrl: "partials/nav.html"
+      templateUrl: 'partials/nav.html'
    };
 });
 
-app.directive("svgInclude", function () {
+app.directive('footer', function () {
    return {
-      templateUrl: "partials/svg.html"
+      template: '<p ng-controller="FooterCtrl" ng-bind-html="footer"></p>'
    };
 });
 
+app.directive('svgInclude', function () {
+   return {
+      templateUrl: 'partials/svg.html'
+   };
+});
 
-app.run(['$rootScope',
-  function ($rootScope) {
-     $rootScope.page = {
-        setTitle: function (title) {
-           this.title = title + ' | Andy Beck';
-        },
-        setDirection: function (direction) {
-           this.direction = direction;
-        },
-        showSubNav: function (subNavShow) {
-           this.subNavShow = subNavShow;
-        }
-     }
-     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-        $rootScope.page.title = current.$$route ? current.$$route.title + ' | Andy Beck' : 'Andy Beck';
-     });
-  }
-]);
+app.directive('clickOutside', ['$document', '$parse', clickOutside]);
+   //github.com/IamAdamJowett/angular-click-outside
+   function clickOutside($document, $parse) {
+      return {
+         restrict: 'A',
+         link: function ($scope, elem, attr) {
+            var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [],
+                fn = $parse(attr['clickOutside']);
+
+            // add the elements id so it is not counted in the click listening
+            if (attr.id !== undefined) {
+               classList.push(attr.id);
+            }
+
+            var eventHandler = function (e) {
+
+               //check if our element already hiden
+               if (angular.element(elem).hasClass("ng-hide")) {
+                  return;
+               }
+
+               var i = 0,
+                   element;
+
+               // if there is no click target, no point going on
+               if (!e || !e.target) {
+                  return;
+               }
+
+               // loop through the available elements, looking for classes in the class list that might match and so will eat
+               for (element = e.target; element; element = element.parentNode) {
+                  var id = element.id,
+                      classNames = element.className,
+                      l = classList.length;
+
+                  // Unwrap SVGAnimatedString
+                  if (classNames && classNames.baseVal !== undefined) {
+                     classNames = classNames.baseVal;
+                  }
+
+                  // loop through the elements id's and classnames looking for exceptions
+                  for (i = 0; i < l; i++) {
+                     // check for id's or classes, but only if they exist in the first place
+                     if ((id !== undefined && id.indexOf(classList[i]) > -1) || (classNames && classNames.indexOf(classList[i]) > -1)) {
+                        // now let's exit out as it is an element that has been defined as being ignored for clicking outside
+                        return;
+                     }
+                  }
+               }
+
+               // if we have got this far, then we are good to go with processing the command passed in via the click-outside attribute
+               return $scope.$apply(function () {
+                  return fn($scope);
+               });
+            };
+
+            // assign the document click handler to a variable so we can un-register it when the directive is destroyed
+            $document.on('click', eventHandler);
+
+            // when the scope is destroyed, clean up the documents click handler as we don't want it hanging around
+            $scope.$on('$destroy', function () {
+               $document.off('click', eventHandler);
+            });
+         }
+      };
+   }
+;
 
 
+/* FACTORIES */
 app.factory('utilities', function () {
    function splitName(fullName) {
       var formData = {},
@@ -105,21 +164,19 @@ app.factory('utilities', function () {
    }
 });
 
-
-app.factory('sortData', ['$http', '$rootScope', '$filter', '$routeParams', function ($http, $rootScope, $filter, $routeParams) {
+app.factory('sortData', ['$http',
+   function ($http) {
    var items = [];
    return {
       getItems: function () {
          return $http.get('data/gallery.json').then(function (response) {
-
             items = response.data;
+
             /* sort items by date in descending order */
             items.sort(function (a, b) {
                return b.date.localeCompare(a.date);
             });
-
-            $rootScope.$broadcast('updateCategory', items);
-
+            
             return {
                items: items
             };
@@ -130,6 +187,27 @@ app.factory('sortData', ['$http', '$rootScope', '$filter', '$routeParams', funct
 }]);
 
 
+/* RUN */
+app.run(['$rootScope', function ($rootScope) {
+   $rootScope.page = {
+        setTitle: function (title) {
+           this.title = title + ' | Andy Beck';
+        },
+        setDirection: function (direction) {
+           this.direction = direction;
+        },
+        showSubNav: function (subNavShow) {
+           this.subNavShow = subNavShow;
+        }
+     }
+     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.page.title = current.$$route ? current.$$route.title + ' | Andy Beck' : 'Andy Beck';
+     });
+  }
+]);
+
+
+/* ANIMATION */
 app.animation('.slide', function () {
    return {
       beforeAddClass: function (element, className, done) {
@@ -144,38 +222,3 @@ app.animation('.slide', function () {
       }
    }
 });
-
-
-//app.animation('.slide', ['$animateCss',
-//  function ($animateCss) {
-//    return {
-//      addClass: function (element, className, done) {
-//        if (className == 'ng-hide') {
-//          var animator = $animateCss(element, {
-//            to: { height: '0px', opacity: 0 }
-//          });
-//          if (animator) {
-//            return animator.start().done(function () {
-//              element[0].style.height = '';
-//              done();
-//            });
-//          }
-//        }
-//        done();
-//      },
-//      removeClass: function (element, className, done) {
-//        if (className == 'ng-hide') {
-//          var height = element[0].offsetHeight;
-//          var animator = $animateCss(element, {
-//            from: { height: '0px', opacity: 0 },
-//            to: { height: height + 'px', opacity: 1 }
-//          });
-//          if (animator) {
-//            return animator.start().done(done);
-//          }
-//        }
-//        done();
-//      }
-//    };
-//  }
-//]);
